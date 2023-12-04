df140868082c29eca3be5ad9c7d85ac192c01d6a
miguelrubiioo <146731914+miguelrubiioo@users.noreply.github.com>
2023-12-04 14:34:40 +0100

    Add files via upload

---
+++ EJERCICIO1exam.js
@@ -0,0 +1,17 @@
+const ej1 = require ("prompt-sync")({ sigint:true })
+
+let NumImput
+let numAleatorio = Math.floor(Math.random{}=7)
+let numAleatorio1 = Math.floor(Math.random{}=7)
+let numAleatorio2 = Math.floor(Math.random{}=7)
+
+let dado = ej1
+
+if (dado == Number(6)) {
+    if (veces == Number(1))
+    console.log ("Has sacado un 1")
+} else{
+    if (veces == Number(2))
+    console.log ("Has sacado un 2")
+}
+
